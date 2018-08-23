3be5b427ac4c2560c889a4f912191ad4ceb80b9f
Koy Saeteurn <kosatun@aol.com>
2018-08-23 19:48:05 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,102 @@
+INSERT INTO users (name, age) VALUES ("a",1);
+INSERT INTO users (name, age) VALUES ("b",2);
+INSERT INTO users (name, age) VALUES ("c",3);
+INSERT INTO users (name, age) VALUES ("d",4);
+INSERT INTO users (name, age) VALUES ("ac",5);
+INSERT INTO users (name, age) VALUES ("av",6);
+INSERT INTO users (name, age) VALUES ("ab",7);
+INSERT INTO users (name, age) VALUES ("ag",8);
+INSERT INTO users (name, age) VALUES ("af",9);
+INSERT INTO users (name, age) VALUES ("av",10);
+INSERT INTO users (name, age) VALUES ("ad",11);
+INSERT INTO users (name, age) VALUES ("ac",12);
+INSERT INTO users (name, age) VALUES ("ac",13);
+INSERT INTO users (name, age) VALUES ("ae",14);
+INSERT INTO users (name, age) VALUES ("al",15);
+INSERT INTO users (name, age) VALUES ("ak",16);
+INSERT INTO users (name, age) VALUES ("a",17);
+INSERT INTO users (name, age) VALUES ("na",18);
+INSERT INTO users (name, age) VALUES ("a",19);
+INSERT INTO users (name, age) VALUES ("ya",20);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("app", "software", 500, 1027, 1100);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("website", "software", 600, 1027, 1100);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("calculator", "math", 300, 1030, 1200);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("kickstarter", "web", 10000, 1000, 1200);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Lollipop", "candy", 100, 1000, 1200);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("MM", "candy", 100, 1000, 1200);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("choco", "candy", 1000, 1000, 1200);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("computer", "hardware", 500000, 1030, 1230);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("keyboard", "hardware", 10000, 1030, 1230);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES (
+"printer", "hardware", 100, 1100, 1200);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (200, 1, 1);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (2002, 2, 1);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (200, 1, 3);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (1200, 2, 1);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (2500, 4, 1);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (200, 5, 1);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (200, 1, 5);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (200, 1, 3);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (200, 1, 2);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (200, 2, 1);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (200, 2, 1);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (200, 2, 2);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (200, 3, 1);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (200, 1, 2);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (200, 5, 5);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (2003, 1, 1);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (2070, 1, 1);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (200, 4, 1);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (200, 1, 4);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (200, 1, 5);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (200, 6, 1);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (200, 1, 6);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (200, 2, 1);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (200, 1, 6);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (200, 1, 1);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (200, 3, 1);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (200, 1, 1);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (2050, 7, 7);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (1200, 1, 7);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (200, 1, 1);
+
